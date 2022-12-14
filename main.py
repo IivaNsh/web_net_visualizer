0de1d6b4a40b03d8dd7dcfbb5dfc5e90bf9facda
IivaNsh <ivansher0226@gmail.com>
2022-12-14 21:49:16 +0300

    now can create web trees

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+import web_node as wbt
 
 base_url = "https://animego.org/"
 
-base_req = requests.get(base_url)
+print("...")
 
-with open('page.txt', 'w') as f:
-    f.write(base_req.text)
-    #print(req.text, file=f)
-
-
-base_page = BeautifulSoup(base_req.text)
-
-links = []
-
-with open('links.txt', 'w') as f:
-    for link in base_page.findAll('a'):
-        link_text= link.get('href')
-        if("https:" in link_text):
-            f.write(link_text+"\n")
-            links.append(link_text)
-        #print(link.get('href'))
-
-
-
-print(f"link \"{base_url}\" --------")
-print("\tpage content saved in file:", "page.txt")
-print("\tlinks found:", len(links))
-print("\tlinks saved in file:", "links.txt")
-
-
+tree = wbt.web_tree(base_url)
+tree.generate(1)
--- web_node.py
+++ web_node.py
@@ -3,22 +3,60 @@ import requests
 from bs4 import BeautifulSoup
 import math
 
-def get_links(page):
-    links = []
-    for link in page.findAll('a'):
+def get_links(bs_page):
+    links = set()
+    for link in bs_page.findAll('a'):
         link_text= link.get('href')
-        if("https:" in link_text):
+        if("https" in link_text):
             #f.write(link_text+"\n")
-            links.append(link_text)
+            links.add(link_text)
+    print("$$$", len(list(links)))
     return links
 
-class web_node:
-    def __init__(self, url, base_url, depth):
+class web_tree:
+    def __init__(self, base_url):
+        self.all_links = set(base_url)
+        self.base_node = web_node(base_url)
+        self.depth = 0
+
+    def generate(self, depth):
         self.depth = depth
-        self.base_url = base_url
+        self.base_node.generate(self.depth, self.all_links)
+
+class web_node:
+    def __init__(self, url):
+        self.url = url
+        self.depth = 0
         self.page = requests.get(url)
-        self.links = get_links(self.page)
+        self.bs_page = BeautifulSoup(self.page.text)
+        self.links = set()
+        self.child_nodes = set()
         self.weight_scale = 1
-        self.weight = self.weight_scale/(1+math.log(len(self.links+1))) 
-        #self.nodes = generate_nodes(self.url, self.depth + 1)
+        self.weight = 1
+
+
+    def generate(self, depth, all_links):
+        
+        self.depth = depth
+        
+        self.links = get_links(self.bs_page)
+        self.weight = self.weight_scale/(1+math.log(len(self.links)+1)) 
+        
+        self.info() 
+        
+        if(self.depth > 0):
+            for next_url in self.links:
+                if not (next_url in all_links):
+                    new_node = web_node(next_url)
+                    new_node.generate(self.depth - 1, all_links)
+                    self.child_nodes.add(new_node)
+                    
+            #for child_node in self.child_nodes:
+
+        all_links.union(self.links)
+
     
+    def info(self):
+        print("  "*(3-self.depth)+"\__"+f" | links - {len(list(self.links))} | url -> {self.url}")
+        
+        
